10799078477a64a208a52a6b30ad50d8a7baa849
user1 <user1@LP-5CD9296CX3.HCLT.CORP.HCL.IN>
2020-02-07 12:32:31 +0530

    http service

--- sandwich/src/app/shared/services/http.service.ts
+++ sandwich/src/app/shared/services/http.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import * as serverConfiguration from '../../assets/serverConfiguration.json';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public getConfigurations(): any {
+    return serverConfiguration['default'][environment.key].ip;
+  }
+
+  private formTheUrl(endpoint): string {
+    return this.getConfigurations() + endpoint;
+  }
+
+  public getRequest(endpoint): Observable<any> {
+    return this.httpClient.get(this.formTheUrl(endpoint));
+  }
+
+  public postRequest(endpoint, payload): Observable<any> {
+    return this.httpClient.post(this.formTheUrl(endpoint), payload);
+  }
 }
